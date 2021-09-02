31c2afe9a88e920dca4e8f8f677b038bf3c8233b
Gowtham-22 <41041646+Gowtham-22@users.noreply.github.com>
2021-09-02 18:08:38 -0400

    Create str_iteration.py

---
+++ str_iteration.py
@@ -0,0 +1,7 @@
+# Write code that uses iteration to print out the length of each element of the list stored in str_list.
+
+str_list = ["hello", "", "goodbye", "wonderful", "I love Python"]
+
+# Write your code here.
+for i in range(len(str_list)):
+    print(len(str_list[i]))
